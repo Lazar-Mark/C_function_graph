2b3d5c226cf0e524a1a486b7699597f3547d3a08
Lazar-Mark <149443529+Lazar-Mark@users.noreply.github.com>
2023-11-01 19:35:51 +0100

    added variable

--- C_function_graph.c
+++ C_function_graph.c
@@ -14,7 +14,6 @@ int velicina_broja(int n){
 
     return brojac;
 }
-//popraviti sto upisuje 42 umesto 24 invertuje
 void dodaj_broj(char* izraz,int broj){
     if(broj<=0)broj=-broj;
     while(1){
@@ -70,14 +69,64 @@ void dodaj_mesto(char* izraz,int broj){
         }
     }
 }
-void resi_izraz(char* izraz){
-    int v;
+int izracunaj_izraz(char* izraz){
+    int rezultat=0;
     int broj=0;
+    while(*izraz !=0){
+
+        if(strncmp(izraz,"-",1)==0){
+            broj=atoi(izraz);
+            rezultat+=broj;
+            izraz+=velicina_broja(broj)+1;
+
+        }
+        else if(strncmp(izraz,"+",1)==0){
+            broj=atoi(izraz+1);
+            rezultat+=broj;
+            izraz+=velicina_broja(broj)+1;
+
+        }
+        else {
+            broj=atoi(izraz);
+            rezultat+=broj;
+            izraz+=velicina_broja(broj);
+
+        }
+
+    }
+
+    return rezultat;
+
+}
+void resi_izraz(char* izraz,int vrednost){
+    int v;
     int v2;
+    int v3;
+    int broj=0;
     int broj2=0;
+    int broj3=0;
 
+    char* temp =izraz;
+    printf("Usao je u resi_izraz\n");
    while(*izraz !=0){
+         printf("while u resi izraz\n");
+        //resavanje x
+        if(*izraz =='x'){
+            izraz--;
+             while(*izraz>='0' && *izraz<='9'){
+                    if(izraz==temp)break;
+                izraz--;
+             }
 
+             broj3=atoi(izraz);
+             izbrisi_karaktere(izraz,velicina_broja(broj3)+1);//+1 za x
+             dodaj_mesto(izraz,velicina_broja(broj3*vrednost));
+             dodaj_broj(izraz+velicina_broja(broj3*vrednost)-1,broj3*vrednost);
+            izraz+=velicina_broja(broj3*vrednost);
+        }
+
+
+        // mnozenje
         if(*izraz=='*'){
 
         broj=atoi(izraz+1);
@@ -95,25 +144,38 @@ void resi_izraz(char* izraz){
 
         dodaj_mesto(izraz,velicina_broja(broj*broj2));
         dodaj_broj(izraz+velicina_broja(broj*broj2),broj*broj2);
-        printf("Proizvod: %d\nVelicina broja: %d\n",broj2*broj,velicina_broja(broj*broj2));
+
         }
 
 
     izraz++;
    }
-
+    printf("izasao je i treba da printa rezultat\n");
+    izraz=temp;
+    printf("Sredjen izraz: %s\n",izraz);
+    printf("Resenje: %d\n",izracunaj_izraz(izraz));
 }
-// primer 2+3*8+3*5
-int main(){
+// primer 2x+2*4+3*5
 
+int main(){
+    printf("Unesi funkciju tipa 2x+4*3+2\n");
     char izraz[128];
     fgets(izraz,128,stdin);
-
     izraz[strlen(izraz)-1]=0;
 
-   // izbrisi_karaktere(izraz+2,3);
-   resi_izraz(izraz);
+    //ovo ne radi
 
-   printf("%s",izraz);
+
+
+    //ovo radi
+        resi_izraz(izraz,5);
+
+
+
+
+
+   //resi_izraz(izraz,2);
+
+
 
 }
